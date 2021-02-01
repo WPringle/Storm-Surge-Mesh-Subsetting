82c25775fdf464d434dbcf068c0efabdb13f3910
William Pringle <wpringle@anl.gov>
2021-02-01 16:27:46 -0600

    removing the common_scripts folder and copying the adcprep run files to all of ATCF, NAM and NDFD folders

---
+++ NDFD/run_adcprep-15.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# run adcprep
+./adcprep << EOF
+NP
+4
+fort.14
+fort.15
+EOF
---
+++ NDFD/run_adcprep-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# run adcprep
+./adcprep << EOF
+NP
+1
+fort.14
+EOF
+./adcprep << EOF
+NP
+2
+EOF

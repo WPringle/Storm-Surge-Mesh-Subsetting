f03457c64eca47e111478f97ed55bc3c09752f79
William Pringle <wpringle@anl.gov>
2021-02-14 14:26:37 -0600

    adding first test for Hurricane Florence on HSOFS mesh

---
+++ tests/HSOFS_Florence/plot_HSOFS_Florence.m
@@ -0,0 +1,85 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Plot_Mesh.m                                                             %
+% Plot the merge mesh                                                     %
+% By William Pringle, Oct 2020 - Jan 2021                                 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clearvars; clc;
+%
+% add paths to data and scripts
+addpath(genpath('~/MATLAB/OceanMesh2D/'))
+
+%% Input Setup
+% Input Storm Name
+stormcode = 'STORMCODE';
+% Input Storm Track
+maxlon = -63;
+try
+   trackfile = [upper(stormcode) '_pts'];
+   shp = m_shaperead(trackfile);
+catch
+   trackfile = [lower(stormcode) '_pts'];
+   shp = m_shaperead(trackfile);
+end
+track = cell2mat(shp.ncst);
+track(track(:,1) > maxlon,:) = [];
+
+% Parameters
+buff = 1.0;       % the buffer for the plot
+bw = 1;           % boundary line width
+tw = 2;           % track line width
+figres = '-r300'; % figure resolution
+
+% Output Plot Names
+outname = 'MESH_STORM';
+
+% Load the mesh
+load([outname '.mat'])
+ms_poly_vec = fine_poly.Vertices;
+
+% Get the bbox from the ms subdomain polygon
+bou = [min(ms_poly_vec)' max(ms_poly_vec)'];
+
+% Making buffer for plot
+bou_buff(:,1) = bou(:,1) - buff;
+bou_buff(:,2) = bou(:,2) + buff;
+
+% Make figure directory and save in there
+mkdir('Figs/')
+outname = ['Figs/' outname];
+
+%% Plot mesh subdomains
+% Plot the mesh quality
+plot(m,'type','qual','subdomain',bou_buff);
+m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'g--','linew',bw);
+m_plot(track(:,1),track(:,2),'g-','linew',tw);
+print([outname '_meshqual.png'],figres,'-dpng')
+
+% Plot the mesh reolution
+plot(m,'type','resomeshlog','subdomain',bou_buff);
+m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'k--','linew',bw);
+m_plot(track(:,1),track(:,2),'k-','linew',tw);
+print([outname '_resomesh.png'],figres,'-dpng')
+
+% Plot the bathy
+plot(m,'type','b','subdomain',bou_buff,'colormap',[11 -10 100]);
+m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'r--','linew',bw);
+m_plot(track(:,1),track(:,2),'r-','linew',tw);
+print([outname '_bathy.png'],figres,'-dpng')
+
+% Plot the f13 attributes
+for ii = 1:m.f13.nAttr
+   attname = m.f13.defval.Atr(ii).AttrName; 
+   plot(m,'type',attname,'subdomain',bou_buff);
+   m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'k--','linew',bw);
+   m_plot(track(:,1),track(:,2),'k-','linew',tw);
+   print([outname '_' attname '.png'],figres,'-dpng')
+end
+
+%% Plot full mesh
+% Plot the triangulation with boundaries
+plot(m,'type','bd');
+m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'m--','linew',bw);
+m_plot(track(:,1),track(:,2),'m-','linew',tw);
+m_text(-95,40,[num2str(length(m.p)/1e6,3) 'M vertices'],'fontsize',12)
+m_text(-95,38,[num2str(length(m.t)/1e6,3) 'M elements'],'fontsize',12)
+print([outname '_tri+ob.png'],figres,'-dpng')
---
+++ tests/HSOFS_Florence/run_HSOFS_Florence.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+####################################################
+# This is a test for the subsetting and merge tool #
+# for Hurricane Florence based on the HSOFS mesh   #
+####################################################
+
+## Load MATLAB module required
+module load matlab
+
+## Unzip input track file
+unzip al062018_best_track.zip 
+
+date > run.timing
+
+# run the merging script
+matlab -nosplash -nodesktop -nodisplay < subset_HSOFS_Florence.m 
+
+date >> run.timing
+
+# plot the merged mesh
+matlab -nosplash -nodesktop -nodisplay < plot_HSFOS_Florence.m
+
+date >> run.timing

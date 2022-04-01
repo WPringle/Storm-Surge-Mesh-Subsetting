2acbc1ab523542e8dbb4139f360e925dc99234e5
William Pringle <wpringle@anl.gov>
2022-04-01 16:43:45 -0500

    modifying track plot function

--- tests/HSOFS_Florence/plot_HSOFS_Florence.m
+++ tests/HSOFS_Florence/plot_HSOFS_Florence.m
@@ -13,6 +13,10 @@ addpath(genpath('OM2DHOME'))
 stormcode = 'AL062018';
 % Input mesh name/Output plot name
 outname = 'HSOFS_Florence';
+% Make figure directory for saving in there
+direc = 'Figs/';
+if ~exist(direc); mkdir(direc); end
+
 % Parameters
 buff = 1.0;       % the buffer for the plot
 bw = 1;           % boundary line width
@@ -35,28 +39,24 @@ bou = [min(ms_poly_vec)' max(ms_poly_vec)'];
 bou_buff(:,1) = bou(:,1) - buff;
 bou_buff(:,2) = bou(:,2) + buff;
 
-% Make figure directory and save in there
-mkdir('Figs/')
-outname = ['Figs/' outname];
-
 %% Plot mesh subdomains
 % Plot the mesh quality
 plot(m,'type','qual','subdomain',bou_buff);
 m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'g--','linew',bw);
 m_plot(track(:,1),track(:,2),'g-','linew',tw);
-print([outname '_meshqual.png'],figres,'-dpng')
+print([direc outname '_meshqual.png'],figres,'-dpng')
 
 % Plot the mesh reolution
 plot(m,'type','resomeshlog','subdomain',bou_buff);
 m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'k--','linew',bw);
 m_plot(track(:,1),track(:,2),'k-','linew',tw);
-print([outname '_resomesh.png'],figres,'-dpng')
+print([direc outname '_resomesh.png'],figres,'-dpng')
 
 % Plot the bathy
 plot(m,'type','b','subdomain',bou_buff,'colormap',[11 -10 100]);
 m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'r--','linew',bw);
 m_plot(track(:,1),track(:,2),'r-','linew',tw);
-print([outname '_bathy.png'],figres,'-dpng')
+print([direc outname '_bathy.png'],figres,'-dpng')
 
 % Plot the f13 attributes
 for ii = 1:m.f13.nAttr
@@ -64,7 +64,7 @@ for ii = 1:m.f13.nAttr
    plot(m,'type',attname,'subdomain',bou_buff);
    m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'k--','linew',bw);
    m_plot(track(:,1),track(:,2),'k-','linew',tw);
-   print([outname '_' attname '.png'],figres,'-dpng')
+   print([direc outname '_' attname '.png'],figres,'-dpng')
 end
 
 %% Plot full mesh
@@ -74,4 +74,4 @@ m_plot(ms_poly_vec(:,1),ms_poly_vec(:,2),'m--','linew',bw);
 m_plot(track(:,1),track(:,2),'m-','linew',tw);
 m_text(-95,40,[num2str(length(m.p)/1e6,3) 'M vertices'],'fontsize',12)
 m_text(-95,38,[num2str(length(m.t)/1e6,3) 'M elements'],'fontsize',12)
-print([outname '_tri+ob.png'],figres,'-dpng')
+print([direc outname '_tri+ob.png'],figres,'-dpng')
